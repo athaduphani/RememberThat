c963b2868fee1c8decf34ae4c1147619a6ef9d6b
Phani Vuyyuru <athaduphani@gmail.com>
2017-06-25 01:13:39 -0400

    update

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ restService.post('/transaction', function(req, res) {
       var purpose = req.body.result && req.body.result.parameters && req.body.result.parameters.purpose ? req.body.result.parameters.purpose : "Seems like some problem. Speak again."
       var vegetable = req.body.result && req.body.result.parameters && req.body.result.parameters.Vegetable ? req.body.result.parameters.Vegetable : "Seems like some problem. Speak again."
       var date = req.body.result && req.body.result.parameters && req.body.result.parameters.date ? req.body.result.parameters.date : "Seems like some problem. Speak again."
-      if (purpose = "save"){
+      // if (purpose = "save"){
         return res.json({
-            speech: "Action is " + action +" Vegetable is "+ vegetable + " Date is " + date,
-            displayText: "Action is " + action +" Vegetable is "+ vegetable + " Date is " + date,
+            speech: "Action is " + purpose +" Vegetable is "+ vegetable + " Date is " + date,
+            displayText: "Action is " + purpose +" Vegetable is "+ vegetable + " Date is " + date,
             source: 'RememberThat'
           });
-      }else if (purpose = "delete") {
-
-      }else{
-        return res.json({
-            speech: "Action is " + action +" Vegetable is "+ vegetable + " Date is " + date,
-            displayText: "Action is " + action +" Vegetable is "+ vegetable + " Date is " + date,
-            source: 'RememberThat'
-      });
-    }
+      // }else if (purpose = "delete") {
+      //
+      // }else{
+    //     return res.json({
+    //         speech: "Action is " + action +" Vegetable is "+ vegetable + " Date is " + date,
+    //         displayText: "Action is " + action +" Vegetable is "+ vegetable + " Date is " + date,
+    //         source: 'RememberThat'
+    //   });
+    // }
     });
     restService.listen((process.env.PORT || 8000), function() {
         console.log("Server up and listening");
