a35dea872b180019df00ae4cf753b72d52eaeb93
Phani Vuyyuru <athaduphani@gmail.com>
2017-06-25 13:01:04 -0400

    dc

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
-// Copyright 2016, Google, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START app]
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
-let ApiAiApp = require('actions-on-google').ApiAiApp;
-let sprintf = require('sprintf-js').sprintf;
+const express = require('express');
+const bodyParser = require('body-parser');
+const restService = express();
 
-const PURPOSE = 'save';
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
+restService.use(bodyParser.json());
 
-// HTTP Cloud Function handler
-exports.transaction = function (req, res) {
-  console.log('headers: ' + JSON.stringify(request.headers));
-  console.log('body: ' + JSON.stringify(request.body));
+restService.post('/transaction', function(req, res) {
+      var purpose = req.body.result && req.body.result.parameters && req.body.result.parameters.purpose ? req.body.result.parameters.purpose : "Seems like some problem. Speak again."
+      var vegetable = req.body.result && req.body.result.parameters && req.body.result.parameters.Vegetable ? req.body.result.parameters.Vegetable : "Seems like some problem. Speak again."
+      var date = req.body.result && req.body.result.parameters && req.body.result.parameters.date ? req.body.result.parameters.date : "Seems like some problem. Speak again."
+      if (purpose = "save"){
+        return res.json({
+            speech: "Purpose is" + purpose + ", vegetable is " + vegetable + ", date is "+ date,
+            displayText: "Purpose is" + purpose + ", vegetable is " + vegetable + ", date is "+ date,
+            source: 'RememberThat'
+      });
+      }else if (purpose = "delete") {
 
-  const app = new ApiAiApp({req, res});
-
-  function save (app) {
-    console.log('save');
-    var purpose = req.body.result && req.body.result.parameters && req.body.result.parameters.purpose ? req.body.result.parameters.purpose : "Seems like some problem. Speak again."
-    var vegetable = req.body.result && req.body.result.parameters && req.body.result.parameters.Vegetable ? req.body.result.parameters.Vegetable : "Seems like some problem. Speak again."
-    var date = req.body.result && req.body.result.parameters && req.body.result.parameters.date ? req.body.result.parameters.date : "Seems like some problem. Speak again."
-    return res.json({
-        speech: "Purpose is" + purpose + ", vegetable is " + Vegetable + ", date is "+ date,
-        displayText: "Purpose is" + purpose + ", vegetable is " + Vegetable + ", date is "+ date,
-        source: 'RememberThat'
-  });
-  }
-  let actionMap = new Map();
-  actionMap.set(PURPOSE, save);
-  app.handleRequest(actionMap);
-};
+      }else{
+        return res.json({
+            speech: "Sorry! Something went wrong. Please try again",
+            displayText: "Sorry! Something went wrong. Please try again",
+            source: 'RememberThat'
+      });
+    }
+    });
+    restService.listen((process.env.PORT || 8000), function() {
+        console.log("Server up and listening");
+    });
