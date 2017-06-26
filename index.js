73a6e5dfc23dcdeb2dfd088339ecd25c17722afb
Phani Vuyyuru <athaduphani@gmail.com>
2017-06-26 19:44:30 -0400

    adding tables

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ var url = "mongodb://aarti:Columbus23@ds139072.mlab.com:39072/heroku_wpdkpvk8";
 restService.post('/transaction', function(req, res) {
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  db.createCollection("users", function(err, res) {
+  db.createCollection("users1", function(err, res) {
     if (err) throw err;
     console.log("Table created!");
     // db.close();
   });
-  // var myobj = { name: "Company Inc1", address: "Highway 371" };
-  // db.collection("users").insertOne(myobj, function(err, res) {
-  //   if (err) throw err;
-  //   console.log("1 record inserted");
-  //   db.close();
-  // });
+  var myobj = { name: "Company Inc1", address: "Highway 371" };
+  db.collection("users").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    db.close();
+  });
 });
 // MongoClient.connect(url, function(err, db) {
 //   if (err) throw err;
