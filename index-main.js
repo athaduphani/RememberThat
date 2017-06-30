7437ff95fd3679dbb6df2b4676d5325421fb3be3
Phani Vuyyuru <athaduphani@gmail.com>
2017-06-30 02:25:28 -0400

    updated

---
+++ index-main.js
@@ -0,0 +1,61 @@
+'use strict';
+// create valiarable
+const express = require('express');
+const bodyParser = require('body-parser');
+const restService = express();
+var mongo = require('mongodb');
+restService.use(bodyParser.urlencoded({extended: true}));
+restService.use(bodyParser.json());
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://aarti:Columbus23@ds139072.mlab.com:39072/heroku_wpdkpvk8";
+var purpose ="";
+restService.post('/transaction', function(req, res) {
+      purpose = req.body.result && req.body.result.parameters && req.body.result.parameters.purpose ? req.body.result.parameters.purpose : "Seems like some problem. Speak again."
+      var saved = "failed"
+      if (purpose = "save"){
+        MongoClient.connect(url, function(err, db) {
+          if (err) throw err;
+          var myobj = req.body;
+          db.collection("transaction").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 record inserted");
+            saved = "success"
+            db.close();
+          });
+        });
+        if (saved = "success"){
+          return res.json({
+            speech: "1 record inserted",
+            displayText: "1 record inserted",
+            source: 'RememberThat'
+          });
+        }else{
+          
+          return res.json({
+            speech: "Sorry! Something went wrong. Please try again",
+            displayText: "Sorry! Something went wrong. Please try again",
+            source: 'RememberThat'
+          });
+        }
+
+    }else if (purpose = "retrieve") {
+      MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var myobj = req.body.result.parameters;
+        db.collection("transaction").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 record inserted");
+          db.close();
+        });
+      });
+      }else{
+        return res.json({
+            speech: "Sorry! Something went wrong. Please try again",
+            displayText: "Sorry! Something went wrong. Please try again",
+            source: 'RememberThat'
+      });
+    }
+    });
+    restService.listen((process.env.PORT || 8000), function() {
+        console.log("Server up and listening");
+    });
--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict';
 // create valiarable
-const express = require('express');
-const bodyParser = require('body-parser');
-const restService = express();
+process.env.DEBUG = 'actions-on-google:*';
+
+let Assistant = require('actions-on-google').ApiAiApp;
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+
+const SAVE = 'save';
+
 var mongo = require('mongodb');
-restService.use(bodyParser.urlencoded({extended: true}));
-restService.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://aarti:Columbus23@ds139072.mlab.com:39072/heroku_wpdkpvk8";
-var purpose ="";
-restService.post('/transaction', function(req, res) {
-      purpose = req.body.result && req.body.result.parameters && req.body.result.parameters.purpose ? req.body.result.parameters.purpose : "Seems like some problem. Speak again."
-      var saved = "failed"
-      if (purpose = "save"){
+// var purpose ="";
+// Start of transaction function
+app.post('/transaction', function(req, res) {
+  const assistant = new Assistant({
+  request: req,
+  response: res
+  });
+      // purpose = req.body.result && req.body.result.parameters && req.body.result.parameters.purpose ? req.body.result.parameters.purpose : "Seems like some problem. Speak again."
+//start save function
+      function save (app){
         MongoClient.connect(url, function(err, db) {
           if (err) throw err;
           var myobj = req.body;
@@ -23,21 +34,9 @@ restService.post('/transaction', function(req, res) {
             db.close();
           });
         });
-        if (saved = "success"){
-          return res.json({
-            speech: "1 record inserted",
-            displayText: "1 record inserted",
-            source: 'RememberThat'
-          });
-        }else{
-          return res.json({
-            speech: "Sorry! Something went wrong. Please try again",
-            displayText: "Sorry! Something went wrong. Please try again",
-            source: 'RememberThat'
-          });
-        }
-
-    }else if (purpose = "retrieve") {
+      } // end save function
+      // start retrieve function
+      function retrieve (app) {
       MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var myobj = req.body.result.parameters;
@@ -47,14 +46,17 @@ restService.post('/transaction', function(req, res) {
           db.close();
         });
       });
-      }else{
-        return res.json({
-            speech: "Sorry! Something went wrong. Please try again",
-            displayText: "Sorry! Something went wrong. Please try again",
-            source: 'RememberThat'
-      });
-    }
+    } // End retrieve function
+    //Start fallback function
+      function fallback (app){
+
+      } // End of fallback function
+// End of Transaction function
+    // Mapping the actions
+    let actionMap = new Map();
+    actionMap.set(SAVE, save);
+    app.handleRequest(actionMap);
     });
-    restService.listen((process.env.PORT || 8000), function() {
+    app.listen((process.env.PORT || 8000), function() {
         console.log("Server up and listening");
     });
