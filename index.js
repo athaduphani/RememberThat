ea1b32f7710c7bc0882379bc6e025f52ac9429bd
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-04 20:18:35 -0400

    3

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ restService.post('/transaction', function(req, res) {
       function save (app){
         var prompt = "Something went wrong. Please try again";
     var transactions = [];
-      for (var i = 0; i < parameters_app.vegetable.length; i++) {
+      for (var i = 0; i < 2; i++) {
          transactions[i] = {
             transactionId: req.body.id,
             SessionId: req.body.sessionId,
