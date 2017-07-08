b6ca36a1a27936f61dc9e560b079f097b52d2c57
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-08 19:22:18 -0400

    try22

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ restService.post('/transaction', function(req, res) {
             expiryDate: "07-31-2017",
             purpose: req.body.result.parameters.purpose
           };
-          items_list = items_list + req.body.result.parameters.Items[i];
+          items_list = items_list + req.body.result.parameters.Items[i] + ', ';
           // if (i = 0) {
           //   items_list = req.body.result.parameters.Items[i];
           // // }else if (i = parameters_app.Items.length-1) {
