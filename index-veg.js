66547835f0bc477cf61a447a2b716d385ba25150
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-08 15:51:24 -0400

    multiple changes

--- index-veg.js
+++ index-veg.js
@@ -7,17 +7,27 @@ let sprintf = require('sprintf-js').sprintf;
 let express = require('express');
 let bodyParser = require('body-parser');
 let restService = express();
+var mongo = require('mongodb');
 
-const SAVE = 'save';
-const RETRIEVE = 'retrieve';
-const WELCOME = 'welcome';
+const SAVE_ACTION = 'save';
+const RETRIEVE_ACTION = 'retrieve';
+const WELCOME_ACTION = 'welcome';
+const DELETE_ACTION = 'delete';
+const MODIFY_ACTION = 'modify';
+const SAVE_YES_ACTION = 'save_yes';
+const SAVE_NO_ACTION = 'save_no';
 const GREETING_PROMPTS = ['Welcome to Dates Bot!', 'Hi! This is Dates Bot.',
     'Welcome back to Dates Bot.'];
 const INVOCATION_PROMPTS = ['I can save, retrieve and update any dates for you. How can I help you today? ', 'How can i help you today'];
 const NO_INPUT_PROMPTS = ['I didn\'t hear it. Can you please repeat it', 'If you\'re still there, please tell me how can I help you',
     'We can stop here. Let\'s talk again soon.'];
-const CONTINUATION_PROMPTS = ['Do you want to save anything else?', 'is there anything I can help you with?','Do you want to save any other dates'];
-var mongo = require('mongodb');
+const CONTINUATION_PROMPTS_SAVE = ['Do you want to save anything else?','is there anything I can help you with?','Do you want to save any other dates']
+
+const SAVE_YES_NO_CONTEXT = 'save_yes_no';
+const RE_PROMPT = ['Great!', 'Awesome!', 'Cool!'];
+const SAVE_RE_INVOCATION_PROMPT = ['What do you want me to save?'];
+const QUIT_PROMPTS = ['Alright, talk to you later then.', 'OK, till next time.','OK, Make sure to ask me if you want any date you saved.'
+    'See you later.', 'OK, Make sure to ask me what items you have and how fresh they are next time'];
 restService.use(bodyParser.urlencoded({extended: true}));
 restService.use(bodyParser.json());
 var MongoClient = require('mongodb').MongoClient;
@@ -66,16 +76,25 @@ restService.post('/transaction', function(req, res) {
       function save (app){
         var prompt = "Something went wrong. Please try again";
     var transactions = [];
-      for (var i = 0; i < parameters_app.Items.length; i++) {
+    var items_list;
+      for (var i = 0; i < parameters_app.Vegetable.length; i++) {
          transactions[i] = {
             transactionId: req.body.id,
             SessionId: req.body.sessionId,
-            item: req.body.result.parameters.Items[i],
+            item: req.body.result.parameters.Vegetable[i],
             type: "Vegetable",
             date: req.body.result.parameters.date,
             expiryDate: "07-31-2017",
             purpose: req.body.result.parameters.purpose
           };
+          if (parameters_app.Vegetable.length==1) {
+            items_list = req.body.result.parameters.Vegetable[i]
+          }else if (i = parameters_app.Vegetable.length-1) {
+            items_list = items_list + " and " + req.body.result.parameters.Vegetable[i];
+          }else {
+            items_list = items_list + "," + req.body.result.parameters.Vegetable[i];
+          }
+
         }
         MongoClient.connect(url, function(err, db) {
           if (err) throw err;
@@ -85,15 +104,15 @@ restService.post('/transaction', function(req, res) {
               db.close();
             });
           });
-          let title = "I saved that you" + req.body.result.parameters.purpose + "on" + req.body.result.parameters.date;
-          prompt = printf(title + ' ' + getRandomPrompt(app, CONTINUATION_PROMPTS));
+          let title = "I saved that you " + req.body.result.parameters.purpose +" " items_list + " on" + req.body.result.parameters.date;
+          prompt = printf(title + ' ' + getRandomPrompt(app, CONTINUATION_PROMPTS_SAVE));
         ask(app, prompt);
       } // end save function
       // start retrieve function
       function retrieve (app) {
       MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        db.collection("transaction").find({$and: [{"type":"Vegetable"},{"item":req.body.result.parameters.items}]}).toArray(function(err, result){
+        db.collection("transaction").find({$and: [{"type":"Vegetable"},{"item":req.body.result.parameters.Vegetable}]}).toArray(function(err, result){
         if (err) throw err;
         console.log(result[0].date);
         db.close();
@@ -105,6 +124,18 @@ restService.post('/transaction', function(req, res) {
         }); // End DB Function
     });
   } // End retrieve function
+  // Start of saveYes function
+  function saveYes (app) {
+    console.log('saveYes');
+    ask(app, printf(getRandomPrompt(app, RE_PROMPT) + ' ' +
+      getRandomPrompt(app, SAVE_RE_INVOCATION_PROMPT)));
+  }// End of saveYes function
+  // Start of saveNo function
+  function saveNo (app) {
+    console.log('saveNo');
+    app.setContext(GAME_CONTEXT, 1);
+    app.tell(printf(getRandomPrompt(app, QUIT_PROMPTS)));
+  } // End of saveNo function
     //Start fallback function
       function fallback (app){
 
@@ -126,9 +157,11 @@ restService.post('/transaction', function(req, res) {
       }
     // Mapping the actions
     let actionMap = new Map();
-    actionMap.set(SAVE, save);
-    actionMap.set(RETRIEVE, retrieve);
-    actionMap.set(WELCOME, welcome);
+    actionMap.set(SAVE_ACTION, save);
+    actionMap.set(RETRIEVE_ACTION, retrieve);
+    actionMap.set(WELCOME_ACTION, welcome);
+    actionMap.set(SAVE_YES_ACTION, saveYes);
+    actionMap.set(SAVE_NO_ACTION, saveNo);
     app.handleRequest(actionMap);
     }); // End of Transaction function
     // app is listening on the port 8000
