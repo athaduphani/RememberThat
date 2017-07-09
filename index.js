b78230a10ed122efd7e7047475545a971b95cc9f
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-09 09:45:37 -0400

    c

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ restService.post('/transaction', function(req, res) {
           db.collection("transaction").find({"SessionId":'a9ac51ae-3dbd-4cd9-91ee-9af3e4796646'}).toArray(function(err, result){
           if (err) throw err;
           // if (result.length = 46) {
-          //   firstTimeUserPrompt = getRandomPrompt(app, FIRST_INTERACTION_EXAMPLES);
+            firstTimeUserPrompt = getRandomPrompt(app, FIRST_INTERACTION_EXAMPLES);
           // }else{
           //   firstTimeUserPrompt = '';
           // }
