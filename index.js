d93361bb4dca24f0908cf50898b8d38b0628ac07
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-09 09:16:00 -0400

    c

--- index.js
+++ index.js
@@ -69,11 +69,10 @@ restService.post('/transaction', function(req, res) {
     var firstTimeUserPrompt = 'asdasd';
         MongoClient.connect(url, function(err, db) {
           if (err) throw err;
-          db.collection("transaction").find({"SessionId":req.body.sessionId}).toArray(function(err, result){
+          db.collection("transaction").find({"SessionId":'a9ac51ae-3dbd-4cd9-91ee-9af3e4796646'}).toArray(function(err, result){
           if (err) throw err;
-          console.log(result[0].date);
-          db.close();
           firstTimeUserPrompt = result[0].date;
+          console.log(firstTimeUserPrompt);
           // let prompt = printf(firstTimeUserPrompt);
           // ask(app, prompt);
           // if (result.length = 0) {
