37dcde0b8217c9982fbd9246fba86403e1d7df73
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-10 22:03:54 -0400

    c

--- index.js
+++ index.js
@@ -150,6 +150,7 @@ restService.post('/transaction', function(req, res) {
             responseForWhat = responseForWhat + result[i].item + ', ';
           }
           }else if(req.body.result.parameters.questionTag == "when"){
+            responseForWhat = '';
             responseForWhen = responseForWhen + 'You Bought ' + result[i].item + ' on '  + result[i].date + '\n.';
           }else{
             responseGeneral = '';
