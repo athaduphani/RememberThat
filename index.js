5a9bed74d21e5fc14e33deff4f7f77a41e486a8e
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-15 11:53:28 -0400

    c

--- index.js
+++ index.js
@@ -199,36 +199,37 @@ restService.post('/transaction', function(req, res) {
                  if (err) throw err;
                  console.log("1 record deleted");
                  db.close();
-                 var responseArray = toArray(res);
-                //  let response = createResponse (res, req.body.result.parameters.Items[0],'',' removed from your items.');
-                // let response = res.value.date + ' length';
-                let response = responseArray.length + ' length';
-                //  let response = req.body.result.parameters.Items[0] + ' removed from your items.';
+                 let response = '';
+                 if(res.value.date == undefined ){
+                   response = 'You don\'t have any ' +  req.body.result.parameters.Items[0];
+                 }ele{
+                    response = req.body.result.parameters.Items[0] + ' removed from your items.';
+                 }
                  let prompt = printf(response + ' ' + getRandomPrompt(app, CONTINUATION_PROMPTS));
                ask(app, prompt);
                });
            });
   } // End Remove Function
-  function createResponse (result, parameter , startStatement, endStatement){
-    var response = ' ';
-    if(result.length == 0 ){
-    response = 'You don\'t have any ' + parameter;
-  }
-  // else if(result.length == 1 ){
-  //   response = parameter + ' removed from your items.';
-  // }else{
-  //     for (var i = 0; i < result.length; i++) {
-  //       if(i == 0){
-  //         response = response + startStatement;
-  //       }else if (i == result.length-1) {
-  //         response = response + ' and '+ req.body.result.parameters.Items[i] + endStatement;
-  //       }else{
-  //         response = response + req.body.result.parameters.Items[i] ;
-  //       }
-  //     }
-  //   }
-    return response;
-  }
+  // function createResponse (result, parameter , startStatement, endStatement){
+  //   var response = ' ';
+  //   if(result.length == 0 ){
+  //   response = 'You don\'t have any ' + parameter;
+  // }
+  // // else if(result.length == 1 ){
+  // //   response = parameter + ' removed from your items.';
+  // // }else{
+  // //     for (var i = 0; i < result.length; i++) {
+  // //       if(i == 0){
+  // //         response = response + startStatement;
+  // //       }else if (i == result.length-1) {
+  // //         response = response + ' and '+ req.body.result.parameters.Items[i] + endStatement;
+  // //       }else{
+  // //         response = response + req.body.result.parameters.Items[i] ;
+  // //       }
+  // //     }
+  // //   }
+  //   return response;
+  // }
   // Start of repeatYes function
   function repeatYes (app) {
     console.log('repeatYes');
