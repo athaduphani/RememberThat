6661faa1612dcb576227f73330b76916d1b510b5
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-18 19:18:33 -0400

    f

--- index.js
+++ index.js
@@ -98,24 +98,28 @@ restService.post('/transaction', function(req, res) {
   } //End Welcome Function
   // start get Type Function
   function getType (item){
-
-    var mongoOutput = MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
+    return new Promise(function(resolve, reject) {
+      MongoClient.connect(url, function(err, db) {
+        if (err) { reject(err); } else { resolve(db); }
+      }
+    }).then(function(db) {
+      return new Promise(function(resolve, reject) {
       db.collection("items_data").find({"name": "Banana"}).toArray(function(err, result){
-      if (err) throw err;
-      db.close();
-      var type = '';
-      if(result.length == 0){
-          console.log(" Type is not found for the item");
-          type = 'Default';
-    }else{
-      type = result[0].type;
-    }
-      return type;
-      }); // End DB Function
-      return db;
-  });
-return mongoOutput;
+          if (err) {
+            reject(err);
+          } else {
+            var type = '';
+            if(result.length == 0){
+                console.log(" Type is not found for the item");
+                type = 'Default';
+          }else{
+            type = result[0].type;
+          }
+            resolve(type);
+          }
+        });
+      });
+    });
   }
 //start save function
       function save (app){
