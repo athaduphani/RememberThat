3ea16670365c6d07df79f2aff62c72a095deb2ee
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-21 20:10:16 -0400

    h

--- db1.js
+++ db1.js
@@ -1,7 +1,6 @@
 var dateFormat = require('dateformat');
 
 var myDate = new Date("07-20-2017");
-// console.log(myDate);
 myDate.setDate(myDate.getDate() + 5);
 var day=dateFormat(myDate, "yyyy-mm-dd");
 
