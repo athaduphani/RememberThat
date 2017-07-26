e13bd7ac7f0e294864f22bc6e25f99c810e46319
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-25 20:59:58 -0400

    f

--- db1.js
+++ db1.js
@@ -3,3 +3,13 @@ var isSingular = require('is-singular')
 console.log(pluralize("i bought tomato"));
 // var try = isSingular("tomato")
 console.log(isSingular("tomatoes"));
+app1();
+var var1 = 1;
+function app1(){
+  console.log("Hello");
+}
+function app2(){
+  if(var1 ==1){
+    app1();
+  }
+}
