aed0441c26f60894f094cf783a37a40d9c425db2
Phani Vuyyuru <athaduphani@gmail.com>
2017-07-31 21:09:45 -0400

    d

--- db1.js
+++ db1.js
@@ -1,35 +1,40 @@
 // var dataMap = require('./data.js');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://aarti:Columbus23@ds139072.mlab.com:39072/heroku_wpdkpvk8";
-var contexts= [
-     {
-       "name": "_actions_on_google_",
-       "parameters": {
-         "printed": "Welcome to Dates Bot! How may I assist you today?",
-         "lastPrompt": "Welcome to Dates Bot! How may I assist you today?"
-       },
-       "lifespan": 100
-     },
-     {
-       "name": "welcome",
-       "parameters": {},
-       "lifespan": 5
-     }
-   ];
-var contexts1 = searchInObject(contexts, "name", "_actions_on_google_");
-console.log(contexts1);
-// if(typeof contexts.parameters != "undefined" && contexts.parameters.resultQuery) {
-//   console.log('we have obj! and val is : ' + obj.key)
-// }
-if(contexts1.parameters.hasOwnProperty('printed')){
-  console.log("true");
-}else {
-  console.log("false");
-}
-function searchInObject(object, searchKey, searchValue) {
-  for (var i in object) {
-    if (object[i][searchKey].indexOf(searchValue) > -1) {
-      return object[i];
+let startStatement = 'You have ';
+let middleStatement = ' which are bought on ';
+let endStatement = '].\n ';
+result = ['Apples', 'Oranges']
+
+// function responseforMultiple(result, startStatement, middleStatement, endStatement){
+  let response = '';
+  let itemName = 'NA';
+  for (var i = 0; i < result.length; i++) {
+    if(result[i].item == itemName){
+      if( i != result.length-1){
+      if(result[i].item == result[i+1].item){
+        response = response + ', ' + result[i].date;
+      }else{
+        response = response + ' and ' + result[i].date;
+      }
+    }else{
+      response = response + ' and ' + result[i].date+ endStatement;
     }
+    }
+    else{
+      if(result.length == 1){
+        response = response + startStatement + result[i].item + middleStatement  + result[i].date+'.\n';
+      }
+      else if(i == 0){
+        response = response + startStatement + result[i].item + middleStatement +'['  + result[i].date;
+      }else if (i == result.length-1) {
+        response = response + endStatement +startStatement + result[i].item + middleStatement  +result[i].date+'.\n';
+      }else {
+        response = response + endStatement +startStatement + result[i].item + middleStatement+'['  +result[i].date;
+      }
+      itemName = result[i].item;
   }
 }
+  // return response;
+  console.log(response);
+// }
